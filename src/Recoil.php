d708692c770b55bcb5e8b2dcad3d502f93c0bbe9
Reinier Kip <reinier@ibuildings.nl>
2014-11-22 16:25:36 +0100

    Add kernel proxy method signatures to Recoil.

--- src/Recoil.php
+++ src/Recoil.php
@@ -4,6 +4,7 @@ namespace Recoil;
 use Recoil\Kernel\Api\KernelApiCall;
 use Recoil\Kernel\Kernel;
 use React\EventLoop\LoopInterface;
+use Recoil\Kernel\Strand\StrandInterface;
 
 /**
  * Public facade for Kernel API calls.
@@ -14,6 +15,22 @@ use React\EventLoop\LoopInterface;
  *
  * The interface {@link Recoil\Kernel\KernelApiInterface} defines the
  * operations that are available; some kernels may provide additional features.
+ *
+ * @method strand() [COROUTINE] Get the strand the coroutine is executing on.
+ * @method kernel() [COROUTINE] Get the coroutine kernel that the current strand is executing on.
+ * @method eventLoop() [COROUTINE] Get the React event-loop that the coroutine kernel is executing on.
+ * @method return_($value) [COROUTINE] Return a value to the calling coroutine.
+ * @method throw_(Exception $exception) [COROUTINE] Throw an exception to the calling coroutine.
+ * @method finally_(callable $callback) [COROUTINE] Register a callback to be invoked when the current coroutine ends.
+ * @method terminate() [COROUTINE] Terminate execution of this strand.
+ * @method sleep(float $timeout) [COROUTINE] Suspend execution for a specified period of time.
+ * @method suspend(callable $callback) [COROUTINE] Suspend execution of the strand until it is resumed manually.
+ * @method timeout(float $timeout, $coroutine) [COROUTINE] Execute a coroutine with a time limit.
+ * @method all(array $coroutines) [COROUTINE] Execute the given coroutines concurrently.
+ * @method noop() [COROUTINE] Resume the strand immediately.
+ * @method cooperate() [COROUTINE] Suspend the strand until the next tick.
+ * @method execute($coroutine) [COROUTINE] Execute a coroutine on its own strand.
+ * @method stop(bool $stopEventLoop = true) [COROUTINE] Stop the coroutine kernel / event-loop.
  */
 abstract class Recoil
 {
