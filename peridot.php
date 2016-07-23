d561f9b041346343fc435ccf53816dde19293fae
James Harris <james.harris@icecave.com.au>
2016-07-23 15:03:25 +1000

    Add peak memory output to peridot config.

--- peridot.php
+++ peridot.php
@@ -7,14 +7,17 @@ use Peridot\Console\Environment;
 use Peridot\Reporter\CodeCoverage\AbstractCodeCoverageReporter;
 use Peridot\Reporter\CodeCoverageReporters;
 
-$autoloader = require __DIR__ . '/vendor/autoload.php';
-// assert((new \Recoil\Dev\Autoloader($autoloader))->register());
+require __DIR__ . '/vendor/autoload.php';
 
 return function (EventEmitterInterface $emitter) {
     (new CodeCoverageReporters($emitter))->register();
 
     $emitter->on('peridot.start', function (Environment $environment) {
         $environment->getDefinition()->getArgument('path')->setDefault('test/suite');
+    });
+
+    $emitter->on('peridot.end', function () {
+        echo 'Peak memory usage: ', number_format(memory_get_peak_usage() / 1024 / 1024, 2), ' MB', PHP_EOL;
     });
 
     $emitter->on('code-coverage.start', function (AbstractCodeCoverageReporter $reporter) {
