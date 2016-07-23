5d8b55b26b3b21e76a2a506e3e0b7881b0e868ac
James Harris <james.harris@icecave.com.au>
2016-07-23 18:11:03 +1000

    Remove memory output.

--- peridot.php
+++ peridot.php
@@ -16,10 +16,6 @@ return function (EventEmitterInterface $emitter) {
         $environment->getDefinition()->getArgument('path')->setDefault('test/suite');
     });
 
-    $emitter->on('peridot.end', function () {
-        echo 'Peak memory usage: ', number_format(memory_get_peak_usage() / 1024 / 1024, 2), ' MB', PHP_EOL;
-    });
-
     $emitter->on('code-coverage.start', function (AbstractCodeCoverageReporter $reporter) {
         $reporter->addDirectoryToWhitelist(__DIR__ . '/src');
     });
