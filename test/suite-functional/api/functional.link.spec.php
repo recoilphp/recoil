ea82bb3c771eee8151c552aaaf199f539ed4ffda
Kevin Millar <koden.km@gmail.com>
2016-08-01 12:06:11 +1000

    More functional tests for link and unlink.

--- test/suite-functional/api/functional.link.spec.php
+++ test/suite-functional/api/functional.link.spec.php
@@ -4,23 +4,193 @@ declare (strict_types = 1); // @codeCoverageIgnore
 
 namespace Recoil;
 
-rit('terminates the linked strand', function () {
-    $strand = yield Recoil::execute(function () {
-        yield 10;
-        expect(false)->to->be->ok('strand was not terminated');
+context('when called with 1 parameter', function () {
+    rit('terminates one strand when the other exits', function () {
+        $strandA = yield Recoil::execute(function () {
+            yield 10;
+        });
+
+        $strandB = yield Recoil::execute(function () use ($strandA) {
+            yield Recoil::link($strandA);
+        });
+
+        yield;
+
+        expect($strandA->hasExited())->to->be->true;
+        expect($strandB->hasExited())->to->be->true;
     });
 
-    yield Recoil::link($strand);
-    yield Recoil::terminate();
+    rit('terminates one strand when the other exits with an exception', function () {
+        $strandA = yield Recoil::execute(function () {
+            yield 10;
+        });
+
+        $strandB = yield Recoil::execute(function () use ($strandA) {
+            yield Recoil::link($strandA);
+            throw new \Exception('<fail>');
+        });
+
+        try {
+            yield Recoil::adopt($strandB);
+            expect(false)->to->be->ok('expected exception was not thrown');
+        } catch (\Exception $e) {
+            yield;  // Allow linked strands to be terminated.
+            expect($strandA->hasExited())->to->be->true;
+            expect($strandB->hasExited())->to->be->true;
+        }
+    });
+
+    context('when a strand is terminated', function () {
+        rit('terminates the linked strand', function () {
+            $strandA = yield Recoil::execute(function () {
+                yield 10;
+            });
+
+            $strandB = yield Recoil::execute(function () use ($strandA) {
+                yield Recoil::link($strandA);
+                yield 10;
+            });
+
+            yield;
+            $strandA->terminate();
+
+            expect($strandA->hasExited())->to->be->true;
+            expect($strandB->hasExited())->to->be->true;
+        });
+
+        rit('does not terminate unlinked strand', function () {
+            $strandA = yield Recoil::execute(function () {
+                yield 10;
+            });
+
+            $strandB = yield Recoil::execute(function () use ($strandA) {
+                yield Recoil::link($strandA);
+                yield Recoil::unlink($strandA);
+                yield 10;
+            });
+
+            yield;
+            $strandA->terminate();
+
+            expect($strandA->hasExited())->to->be->true;
+            expect($strandB->hasExited())->to->be->false;
+
+            $strandB->terminate();
+        });
+    });
+
+    rit('creates bidirectional links', function () {
+        $strandA = yield Recoil::execute(function () {
+            yield 10;
+        });
+
+        $strandB = yield Recoil::execute(function () use ($strandA) {
+            yield Recoil::link($strandA);
+            yield 10;
+        });
+
+        yield;
+        $strandB->terminate();
+
+        expect($strandA->hasExited())->to->be->true;
+        expect($strandB->hasExited())->to->be->true;
+    });
 });
 
-rit('does not terminate an unlinked strand', function () {
-    $strand = yield Recoil::execute(function () {
+context('when called with 2 parameters', function () {
+    rit('terminates one strand when the other exits', function () {
+        $strandA = yield Recoil::execute(function () {
+            yield 10;
+        });
+
+        $strandB = yield Recoil::execute(function () {
+            yield Recoil::suspend();
+        });
+
         yield;
+        yield Recoil::link($strandA, $strandB);
+        yield $strandB->send();
+
+        expect($strandA->hasExited())->to->be->true;
+        expect($strandB->hasExited())->to->be->true;
     });
 
-    yield Recoil::link($strand);
-    yield Recoil::unlink($strand);
-    yield Recoil::terminate();
-    expect($strand->hasExited())->to->be->false;
+     rit('terminates one strand when the other exits with an exception', function () {
+        $strandA = yield Recoil::execute(function () {
+            yield 10;
+        });
+
+        $strandB = yield Recoil::execute(function () use ($strandA) {
+            yield;
+            throw new \Exception('<fail>');
+        });
+
+        yield;
+        yield Recoil::link($strandA, $strandB);
+
+        try {
+            yield Recoil::adopt($strandB);
+            expect(false)->to->be->ok('expected exception was not thrown');
+        } catch (\Exception $e) {
+            yield;  // Allow linked strands to be terminated.
+            expect($strandA->hasExited())->to->be->true;
+            expect($strandB->hasExited())->to->be->true;
+        }
+     });
+
+    context('when a strand is terminated', function () {
+        rit('terminates the linked strand', function () {
+            $strandA = yield Recoil::execute(function () {
+                yield 10;
+            });
+
+            $strandB = yield Recoil::execute(function () {
+                yield 10;
+            });
+
+            yield;
+            yield Recoil::link($strandA, $strandB);
+            $strandA->terminate();
+
+            expect($strandA->hasExited())->to->be->true;
+            expect($strandB->hasExited())->to->be->true;
+        });
+
+        rit('does not terminate unlinked strand', function () {
+            $strandA = yield Recoil::execute(function () {
+                yield 10;
+            });
+
+            $strandB = yield Recoil::execute(function () {
+                yield 10;
+            });
+
+            yield;
+            yield Recoil::link($strandA, $strandB);
+            yield Recoil::unlink($strandA, $strandB);
+            $strandA->terminate();
+
+            expect($strandA->hasExited())->to->be->true;
+            expect($strandB->hasExited())->to->be->false;
+
+            $strandB->terminate();
+        });
+    });
+
+    rit('creates bidirectional links', function () {
+        $strandA = yield Recoil::execute(function () {
+            yield 10;
+        });
+
+        $strandB = yield Recoil::execute(function () {
+            yield 10;
+        });
+
+        yield;
+        yield Recoil::link($strandA, $strandB);
+        $strandB->terminate();
+
+        expect($strandA->hasExited())->to->be->true;
+        expect($strandB->hasExited())->to->be->true;
+    });
 });
