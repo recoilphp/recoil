1a8541027cd3ea2f3511e3d423ce866d328b0b01
James Harris <james.harris@icecave.com.au>
2016-12-07 10:20:13 +1000

    Added unit tests for `ApiTrait::adopt()` and `stop()`.

--- test/suite/unit/Kernel/ApiTrait.spec.php
+++ test/suite/unit/Kernel/ApiTrait.spec.php
@@ -376,6 +376,16 @@ describe(ApiTrait::class, function () {
         });
     });
 
+    describe('->stop()', function () {
+        it('stops the kernel', function () {
+            $this->subject->get()->stop(
+                $this->strand->get()
+            );
+
+            $this->kernel->stop->called();
+        });
+    });
+
     describe('->link()', function () {
         it('links both strands', function () {
             $this->subject->get()->link(
@@ -391,7 +401,7 @@ describe(ApiTrait::class, function () {
             );
         });
 
-        it('uses the current strand second strand is not provided', function () {
+        it('uses the current strand if the second strand is not provided', function () {
             $this->subject->get()->link(
                 $this->strand->get(),
                 $this->substrand1->get()
@@ -420,7 +430,7 @@ describe(ApiTrait::class, function () {
             );
         });
 
-        it('uses the current strand second strand is not provided', function () {
+        it('uses the current strand if the second strand is not provided', function () {
             $this->subject->get()->unlink(
                 $this->strand->get(),
                 $this->substrand1->get()
@@ -430,6 +440,35 @@ describe(ApiTrait::class, function () {
                 $this->substrand1->unlink->calledWith($this->strand),
                 $this->strand->unlink->calledWith($this->substrand1),
                 $this->strand->send->called()
+            );
+        });
+    });
+
+    describe('->adopt()', function () {
+        it('sets the substrands primary listener to the current strand', function () {
+            $this->subject->get()->adopt(
+                $this->strand->get(),
+                $this->substrand1->get()
+            );
+
+            $this->substrand1->setPrimaryListener->calledWith($this->strand);
+        });
+
+        it('terminates the substrand when the current strand is terminated', function () {
+            $this->subject->get()->adopt(
+                $this->strand->get(),
+                $this->substrand1->get()
+            );
+
+            $terminator = $this->strand->setTerminator->calledWith('~')->firstCall()->argument();
+            expect($terminator)->to->satisfy('is_callable');
+
+            $this->substrand1->terminate->never()->called();
+            $terminator();
+
+            Phony::inOrder(
+                $this->substrand1->clearPrimaryLIstener->called(),
+                $this->substrand1->terminate->called()
             );
         });
     });
